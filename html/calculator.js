46a616497c652767f275d695886be072f865fed3
Muhammad Zubair Khan <161020865+meZubairKhan@users.noreply.github.com>
2024-10-11 21:46:03 +0500

    Add files via upload

---
+++ html/calculator.js
@@ -0,0 +1,7 @@
+let display = document.querySelector('display');
+display.value = 100;
+
+let button = document.getElementsByClassName('btn');
+button.style.border = '3px solid red';
+
+let heading = get.querySelector('portfolio');
